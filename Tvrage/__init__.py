1fbeff794fcce9e2e962c2c192dc5f266618b02d
staticsafe <staticsafe@entropynet.net>
2012-06-03 00:57:16 -0400

    added a commands - genre and nextepisode.

--- Tvrage/__init__.py
+++ Tvrage/__init__.py
@@ -15,20 +15,21 @@ import supybot.world as world
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = ""
+__version__ = "0.1"
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.authors.unknown
+__author__ = supybot.Author('staticsafe')
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = '' # 'http://supybot.com/Members/yourname/Tvrage/download'
+__url__ = 'https://github.com/staticsafe/supybot-plugins' # 'http://supybot.com/Members/yourname/Tvrage/download'
 
 import config
 import plugin
+import tvrage # Install from pip with pip install python-tvrage
 reload(plugin) # In case we're being reloaded.
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
--- Tvrage/plugin.py
+++ Tvrage/plugin.py
@@ -11,7 +11,8 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 from supybot.i18n import PluginInternationalization, internationalizeDocstring
-
+from tvrage import quickinfo
+import tvrage.api
 _ = PluginInternationalization('Tvrage')
 
 @internationalizeDocstring
@@ -19,9 +20,32 @@ class Tvrage(callbacks.Plugin):
     """Add the help for "@plugin help Tvrage" here
     This should describe *how* to use this plugin."""
     threaded = True
+    
+    #def show(self, irc, msg, args, query):
+    #    """<show>
 
+    #    Shows the information about requested TV show.
+    #    """
+    #    answer = quickinfo.fetch(query)
+    #    irc.reply(answer)
+    #show = wrap(show, ['text'])
+
+    def genre(self, irc, msg, args, show):
+        """<show>
+        Shows the genre about TV show
+        """
+        genrereply = tvrage.api.Show(show)
+        irc.reply(genrereply.genres)
+    genre = wrap(genre, ['text'])
+
+    def nextepisode(self, irc, msg, args, show):
+        """<show>
+        Shows the next episode of show
+        """
+        nextreply = tvrage.api.Show(show)
+        irc.reply(nextreply.next_episode)
+    nextepisode = wrap(nextepisode, ['text'])
 
 Class = Tvrage
-
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
